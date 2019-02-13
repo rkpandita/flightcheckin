b5327ba728d9ab6b8d886b2f8ae70c55543a2c83
Krishan Pandita Raman (Supplier) <raman.krishan.pandita@supplier.volvo.com>
2019-02-13 19:37:37 +0530

    Fixed issues reported in SonarLint report

--- src/main/java/com/raman/flightcheckin/controllers/CheckinController.java
+++ src/main/java/com/raman/flightcheckin/controllers/CheckinController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.raman.flightcheckin.integration.ReservationRestClient;
@@ -16,19 +17,19 @@ public class CheckinController {
 	@Autowired
 	ReservationRestClient restClient;
 	
-	@RequestMapping("/showStartCheckin")
+	@RequestMapping(value = "/showStartCheckin", method = RequestMethod.GET)
 	public String showStartCheckin() {
 		return "startCheckin";
 	}
 
-	@RequestMapping("/startCheckin")
+	@RequestMapping(value = "/startCheckin", method = RequestMethod.GET)
 	public String startCheckin(@RequestParam("reservationId") Long reservationId, ModelMap modelMap) {
 		Reservation reservation = restClient.findReservation(reservationId);
 		modelMap.addAttribute("reservation", reservation);
 		return "displayReservationDetails";
 	}
 	
-	@RequestMapping("/completeCheckin")
+	@RequestMapping(value = "/completeCheckin", method = RequestMethod.GET)
 	public String completeCheckIn(@RequestParam("reservationId") Long reservationId, @RequestParam("noOfBags") int noOfBags) {
 		ReservationUpdateRequest request = new ReservationUpdateRequest();
 		request.setCheckedIn(true);
